f1086693a9f81818477e480dd0a36057ae0bec00
IkeyBenz <ikey.benz@gmail.com>
2020-03-01 03:38:26 -0800

    [cleanup] began fixing declaration file. need to do more research

--- src/core/index.d.ts
+++ src/core/index.d.ts
@@ -1,65 +1,63 @@
-declare module "@modi/core" {
-	export class ModiGame {
-		static Events: Object;
-		deck: Deck;
-		players: Player[];
-		playersAlive: Player[];
-		constructor(players: Player[]);
-		start(): Promise<void>;
-	}
-	export class Player {
-		username: string;
-		lives: number;
-		controller: IPlayer;
-		card?: Card;
-		constructor(name: string, controller: IPlayer);
-		wantsToSwap(): Promise<boolean>;
-		tradeCardsWith(other: Player): Card;
-		recieveCard(card: Card): Card;
-		removeCard(): Card | undefined;
-		loseLife(): void;
-	}
-	export interface IPlayer {
-		getMove(): Promise<PlayerMove>;
-	}
-	export enum PlayerMove {
-		Stick,
-		Swap,
-	}
-	export enum Suit {
-		Spades,
-		Hearts,
-		Clubs,
-		Diamonds,
-	}
-	export enum Rank {
-		Ace,
-		Two,
-		Three,
-		Four,
-		Five,
-		Six,
-		Seven,
-		Eight,
-		Nine,
-		Ten,
-		Jack,
-		Queen,
-		King,
-	}
-	export class Card {
-		suit: Suit;
-		rank: Rank;
-		constructor(suit: Suit, rank: Rank);
-		value(): number;
-	}
-	export class Deck {
-		cards: Card[];
-		trash: Card[];
-		constructor();
-		shuffle(): void;
-		dealCard(): Card;
-		addToTrash(card: Card): Card;
-		reload(): Card[];
-	}	
+declare class ModiGame {
+  static Events: object;
+  deck: Deck;
+  players: Player[];
+  playersAlive: Player[];
+  constructor(players: Player[]);
+  start(): Promise<void>;
+}
+declare interface Player {
+  username: string;
+  lives: number;
+  controller: IPlayer;
+  card?: Card;
+  constructor(name: string, controller: IPlayer);
+  wantsToSwap(): Promise<boolean>;
+  tradeCardsWith(other: Player): Card;
+  recieveCard(card: Card): Card;
+  removeCard(): Card | undefined;
+  loseLife(): void;
+}
+declare interface IPlayer {
+  getMove(): Promise<PlayerMove>;
+}
+declare enum PlayerMove {
+  Stick,
+  Swap
+}
+declare enum Suit {
+  Spades,
+  Hearts,
+  Clubs,
+  Diamonds
+}
+declare enum Rank {
+  Ace = 1,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+}
+declare class Card {
+  suit: Suit;
+  rank: Rank;
+  constructor(suit: Suit, rank: Rank);
+  value(): number;
+}
+declare class Deck {
+  cards: Card[];
+  trash: Card[];
+  constructor();
+  shuffle(): void;
+  dealCard(): Card;
+  addToTrash(card: Card): Card;
+  reload(): Card[];
 }
